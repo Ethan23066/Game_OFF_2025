998040859a1db015f2225be84667e011d4ae2377
Ethan23066 <ethanjoyel@gmail.com>
2025-11-08 21:00:46 +0100

    files of launch game

--- player.py
+++ player.py
@@ -1,10 +1,14 @@
 from settings_game import WIDTH
+from bullet import Bullet
+from assets import load_sprite  # Assure-toi que le nom du fichier est bien assets.py
 
 class Player:
     def __init__(self, image):
         self.image = image
         self.rect = self.image.get_rect(midbottom=(WIDTH // 2, 580))
         self.speed = 5
+        self.bullet_speed = 500
+        self.bullet_sprite = load_sprite("bullet_blue.png")
 
     def move_left(self):
         self.rect.x -= self.speed
@@ -23,6 +27,11 @@ class Player:
         return self.rect.center
 
     def fire(self):
-        # Optionnel : retourne un bullet à partir du centre
-        from bullet import Bullet
-        return Bullet(self.rect.centerx, self.rect.top)
+        # Tire un bullet vers le haut
+        return Bullet(
+            x=self.rect.centerx,
+            y=self.rect.top,
+            direction=(0, -1),
+            speed=self.bullet_speed,
+            sprite_name="bullet_blue.png"
+        )
